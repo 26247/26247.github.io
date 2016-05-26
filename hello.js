f57bb40122ed11ee4590666cc01fcded1bcf2b96
26247 <26247@protonmail.com>
2016-05-26 22:20:51 +0900

    animation

--- hello.js
+++ hello.js
@@ -1,7 +1,17 @@
 var ctx = document.getElementById("helloCanvas").getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 20, 80, 80);
-ctx.fillStyle = "#00FF00";
-ctx.fill();
-ctx.closePath();
+var x = 0;
+var y = 0;
+
+function draw(){
+	ctx.beginPath();
+	ctx.rect(20, 20, 20 + x, 20 + y);
+	ctx.fillStyle = "#00FF00";
+	ctx.fill();
+	ctx.closePath();
+
+	x++;
+	y++;
+}
+
+setInterval(draw, 10);
