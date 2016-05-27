5ee82245d70ef0a566d86aadaa6adbeb1b9906ff
26247 <26247@protonmail.com>
2016-05-26 22:12:19 -0700

    more readable

--- hello.js
+++ hello.js
@@ -10,56 +10,56 @@ var pressedUp = 0;
 var pressedDown = 0;
 
 function keyDownEvent(event){
-	if(event.key == 'a'){
-		pressedLeft = 1;
-	}
-	if(event.key == 'd'){
-		pressedRight = 1;
-	}
-	if(event.key == 'w'){
-		pressedUp = 1;
-	}
-	if(event.key == 's'){
-		pressedDown = 1;
-	}
+    if(event.key == 'a'){
+        pressedLeft = 1;
+    }
+    if(event.key == 'd'){
+        pressedRight = 1;
+    }
+    if(event.key == 'w'){
+        pressedUp = 1;
+    }
+    if(event.key == 's'){
+        pressedDown = 1;
+    }
 }
 
 document.addEventListener("keydown", keyDownEvent, false);
 
 function keyUpEvent(event){
-	if(event.key == 'a'){
-		pressedLeft = 0;
-	}
-	if(event.key == 'd'){
-		pressedRight = 0;
-	}
-	if(event.key == 'w'){
-		pressedUp = 0;
-	}
-	if(event.key == 's'){
-		pressedDown = 0;
-	}
+    if(event.key == 'a'){
+        pressedLeft = 0;
+    }
+    if(event.key == 'd'){
+        pressedRight = 0;
+    }
+    if(event.key == 'w'){
+        pressedUp = 0;
+    }
+    if(event.key == 's'){
+        pressedDown = 0;
+    }
 }
 
 document.addEventListener("keyup", keyUpEvent, false);
 
 function draw(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = "#00FF00";
-	ctx.fillRect(20 + x, 20 + y, 20, 20);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#00FF00";
+    ctx.fillRect(20 + x, 20 + y, 20, 20);
 
-	if(pressedLeft == true){
-		x -= 2;
-	}
-	if(pressedRight == true){
-		x += 2;
-	}
-	if(pressedUp == true){
-		y -= 2;
-	}
-	if(pressedDown == true){
-		y += 2;
-	}
+    if(pressedLeft == true){
+        x -= 2;
+    }
+    if(pressedRight == true){
+        x += 2;
+    }
+    if(pressedUp == true){
+        y -= 2;
+    }
+    if(pressedDown == true){
+        y += 2;
+    }
 }
 
 setInterval(draw, 10);
