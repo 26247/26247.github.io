527f0ce31a884683853e7837149a225e59e8ee63
26247 <26247@protonmail.com>
2016-05-27 02:29:58 -0700

    make it smoother

--- hello.js
+++ hello.js
@@ -3,6 +3,8 @@ var ctx = canvas.getContext("2d");
 
 var x = 0;
 var y = 0;
+var dx = 0;
+var dy = 0;
 
 var pressedLeft = 0;
 var pressedRight = 0;
@@ -33,33 +35,30 @@ function keyUpEvent(event){
     if(event.key == 'd'){
         pressedRight = 0;
     }
-    if(event.key == 'w'){
-        pressedUp = 0;
-    }
     if(event.key == 's'){
         pressedDown = 0;
+    }
+    if(event.key == 'w'){
+        pressedUp = 0;
     }
 }
 
 document.addEventListener("keyup", keyUpEvent, false);
 
 function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#00FF00";
     ctx.fillRect(20 + x, 20 + y, 20, 20);
 
-    if(pressedLeft == true){
-        x -= 2;
-    }
-    if(pressedRight == true){
-        x += 2;
-    }
-    if(pressedUp == true){
-        y -= 2;
-    }
-    if(pressedDown == true){
-        y += 2;
-    }
+    dx += pressedRight - pressedLeft;
+    dy += pressedDown - pressedUp;
+
+    dx *= 0.9;
+    dy *= 0.9;
+
+    x += dx;
+    y += dy;
 }
 
-setInterval(draw, 10);
+setInterval(draw, 20);
